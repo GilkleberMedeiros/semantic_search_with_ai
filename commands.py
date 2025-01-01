4b099cd369c40dbe94a1cdf328c513919e2a8a4d
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-01-01 17:51:07 -0300

    Separando as dependências de commands em um arquivo separado.

--- commands.py
+++ commands.py
@@ -1,48 +1,17 @@
-from langchain_community.document_loaders import PyPDFLoader, ChatGPTLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain_community.vectorstores import FAISS
-import faiss
-from langchain_community.docstore import InMemoryDocstore
-from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
-from langchain_google_genai.llms import GoogleGenerativeAI
 from langchain_core.documents import Document
 
 from configs import get_configs, get_google_api_key
+from dependecies import chat_model, vector_store, text_spliter
 
 
 # TODO: Extract configs from a json config file.
-# TODO: Put object dependencies loading in other file.
 # TODO: Make load_documents func get loader class from dict map with .suf: Loader
 
 GOOGLE_API_KEY = get_google_api_key()
 configs = get_configs()
 
-chat_model = GoogleGenerativeAI(
-    model=configs.model_chat,
-    api_key=GOOGLE_API_KEY,
-    temperature=configs.chat_model_temperature,
-    top_p=configs.chat_model_top_p,
-    top_k=configs.chat_model_top_k,
-)
-embeddings_model = GoogleGenerativeAIEmbeddings(
-    model=configs.model_embeddings, 
-    google_api_key=GOOGLE_API_KEY,
-    )
-
-index = faiss.IndexFlatL2(len(embeddings_model.embed_query("hello world")))
-vector_store = FAISS(
-    embedding_function=embeddings_model,
-    index=index,
-    docstore=InMemoryDocstore(),
-    index_to_docstore_id={},
-)
-
-text_spliter = CharacterTextSplitter(
-    chunk_size=configs.text_splitter_chunk_size, 
-    chunk_overlap=configs.text_splitter_chunk_overlap,
-    length_function=configs.text_splitter_length_function,
-)
 
 # train command
 def train(argv: list[str]) -> None:
---
+++ dependecies.py
@@ -0,0 +1,38 @@
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_google_genai.llms import GoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
+import faiss
+from langchain_community.docstore import InMemoryDocstore
+from langchain.text_splitter import CharacterTextSplitter
+
+from configs import get_configs, get_google_api_key
+
+
+GOOGLE_API_KEY = get_google_api_key()
+configs = get_configs()
+
+chat_model = GoogleGenerativeAI(
+    model=configs.model_chat,
+    api_key=GOOGLE_API_KEY,
+    temperature=configs.chat_model_temperature,
+    top_p=configs.chat_model_top_p,
+    top_k=configs.chat_model_top_k,
+)
+embeddings_model = GoogleGenerativeAIEmbeddings(
+    model=configs.model_embeddings, 
+    google_api_key=GOOGLE_API_KEY,
+)
+
+index = faiss.IndexFlatL2(len(embeddings_model.embed_query("hello world")))
+vector_store = FAISS(
+    embedding_function=embeddings_model,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+text_spliter = CharacterTextSplitter(
+    chunk_size=configs.text_splitter_chunk_size, 
+    chunk_overlap=configs.text_splitter_chunk_overlap,
+    length_function=configs.text_splitter_length_function,
+)
